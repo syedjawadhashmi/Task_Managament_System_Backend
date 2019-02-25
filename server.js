96eea44c899a6d73c473cfe30b0d1a884294cf99
syedjawadhashmi <sjawad143@yahoo.com>
2019-02-25 18:25:41 +0500

    Deploying on heroku

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use('/customers', require('./customers/customer.controller'));
 app.use('/projects', require('./projects/project.controller'));
 app.use('/comments', require('./comments/comment.controller'));
 
+console.log('Deploy to Heroku');
+
 // global error handler
 app.use(errorHandler);
 
