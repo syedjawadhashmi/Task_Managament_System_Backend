6d031f1807739befe074e17bd5fd5970fc5996c3
syedjawadhashmi <sjawad143@yahoo.com>
2019-02-25 19:54:54 +0500

    Project schema updated

--- projects/project.model.js
+++ projects/project.model.js
@@ -3,13 +3,17 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     name: { type: String, unique: true, required: true },
-    category: { type: String },
-    ticketSummary: { type: String },
+    rate: { type: Number },
+    rateUnit: { type: String },                   // Month / day / hourly
+    description: { type: String },
+    currency: { type: String },
     status: { type: Number },
-    number: { type: String },
-    assigned: { type: Schema.Types.ObjectId, ref: 'customers' },
+    startDate: { type: Date },
+    estimatedEndDate: { type: Date },
+    actualEndDate: { type: Date },
+    code: { type: String },
     priority: { type: Number },
-    deadline: { type: Date, default: Date.now },
+    assigned: { type: Schema.Types.ObjectId, ref: 'customers' },
     customer: { type: Schema.Types.ObjectId, ref: 'customers' },
     lastUpdated: { type: Date, default: Date.now },
     createdAt: { type: Date, default: Date.now },
